95f4276f99601106f4a47a2e09a6988e4e7910c2
Master Tres <tresbednarz@gmail.com>
2019-07-17 02:19:44 -0400

    Added Feature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,27 +1,34 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
   const url =
     "https://api.darksky.net/forecast/75eba5b5a9e416e571688bc0e937d5a5/" +
-    (latitude) +
+    latitude +
     "," +
-   (longitude) +
+    longitude +
     "?units=us";
 
-  request({ url, json:true }, (error, {body}) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to location services");
     } else if (body.error) {
       callback("unable to find location");
     } else {
-      callback(undefined, 
-        body.daily.data[0].summary + 'It is currently ' +
-         body.currently.temperature + ' degrees out. The high today will be' + 
-         body.daily.temperatureMax + ' with a low of '+ body.daily.temperatureMin + 
-         'there is a ' +
-         body.currently.precipProbability + '% chance of rain.')
-}
-})
-}
+      callback(
+        undefined,
+        body.daily.data[0].summary +  
+          "It is currently " +
+          body.currently.temperature +
+          "°F out. The high today will be " +
+          body.daily.data[0].temperatureMax + 
+          "°F with a low of " +
+          body.daily.data[0].temperatureMin +
+          "°F. There is a " +
+          body.currently.precipProbability +
+          "% chance of rain."
+      );
+    }
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
